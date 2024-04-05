5c88310a99029ad3f0804f54ca2c401ddbb5901f
ignatjevvv <k.ignatjevvv@gmail.com>
2024-04-05 20:25:05 +0300

    improve

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-'use strict';
-
-let totalSumLabel = document.querySelector('.home__data-accumulated'),
+document.addEventListener('DOMContentLoaded', () => {
+  let totalSumLabel = document.querySelector('.home__data-accumulated'),
     remainingAmountLabel = document.querySelector('.home__data-left'),
     finishSumLabel = document.querySelector('.home__data-finish'),
     cashInput = document.getElementById('cashInput'),
@@ -8,8 +7,7 @@ let totalSumLabel = document.querySelector('.home__data-accumulated'),
     usdLabel = document.querySelector('.home__data-usd'),
     rateSellUsd = document.querySelector('.home__data-rate-sell');
 
-let progressBar = {
-
+  let progressBar = {
     totalSum: 0,
     remainingAmount: 0,
     finishSum: 5e5,
@@ -18,75 +16,81 @@ let progressBar = {
     typeOperation: '',
 
     calcSumToFinish() {
-        this.remainingAmount = this.finishSum - this.totalSum;
+      this.remainingAmount = this.finishSum - this.totalSum;
 
-        if (this.remainingAmount < 0) {
-            this.remainingAmount = 0;
-        }
+      if (this.remainingAmount < 0) {
+        this.remainingAmount = 0;
+      }
 
-        if (this.totalSum <= 0) {
-            this.totalSum = 0;
-        }
+      if (this.totalSum <= 0) {
+        this.totalSum = 0;
+      }
 
-        saveDateLocalStor();
-        saveHistoryOperation();
-        this.renderInfo();
-        this.renderHistory();
+      saveDateLocalStor();
+      saveHistoryOperation();
+      this.renderInfo();
+      this.renderHistory();
     },
 
     calcProcentBar() {
-        if (this.totalSum > this.finishSum) {
-            return 100;
-        }
+      if (this.totalSum > this.finishSum) {
+        return 100;
+      }
 
-        if (this.totalSum <= 0) {
-            return 0;
-        }
+      if (this.totalSum <= 0) {
+        return 0;
+      }
 
-        return Math.trunc((100 * this.totalSum) / this.finishSum);
+      return Math.trunc((100 * this.totalSum) / this.finishSum);
     },
 
     remCash() {
-        this.totalSum -= +cashInput.value;
-        this.typeOperation = '-';
-        this.calcSumToFinish();
+      this.totalSum -= +cashInput.value;
+      this.typeOperation = '-';
+      this.calcSumToFinish();
     },
 
     setCash() {
-        this.totalSum += +cashInput.value;
-        this.typeOperation = '+';
-        this.calcSumToFinish();
+      this.totalSum += +cashInput.value;
+      this.typeOperation = '+';
+      this.calcSumToFinish();
     },
 
     renderInfo() {
-        totalSumLabel.innerHTML = this.totalSum;
+      totalSumLabel.innerHTML = this.totalSum;
 
-        this.totalSum == 0 ?
-        remainingAmountLabel.innerHTML = this.finishSum :
-        remainingAmountLabel.innerHTML = this.remainingAmount;
+      this.totalSum == 0
+        ? (remainingAmountLabel.innerHTML = this.finishSum)
+        : (remainingAmountLabel.innerHTML = this.remainingAmount);
 
-        progressLine.style.width = this.calcProcentBar() + '%';
-        usdLabel.innerHTML = this.convertToUSD();
-        rateSellUsd.innerHTML = this.rateSallUSD;
-        finishSumLabel.innerHTML = this.finishSum;
+      progressLine.style.width = this.calcProcentBar() + '%';
+      usdLabel.innerHTML = this.convertToUSD();
+      rateSellUsd.innerHTML = this.rateSallUSD
+        ? Math.round(parseFloat(this.rateSallUSD) * 100) / 100
+        : '-';
+      finishSumLabel.innerHTML = this.finishSum;
 
-        this.renderHistory();
-        cashInput.value = '';
+      this.renderHistory();
+      cashInput.value = '';
     },
 
     convertToUSD() {
-        return (this.totalSum / this.rateSallUSD).toFixed(2);
+      return this.rateSallUSD
+        ? (this.totalSum / this.rateSallUSD).toFixed(2)
+        : '-';
     },
 
     renderHistory() {
-        let arrHistory = JSON.parse('['+localStorage.history+']');
-        document.querySelector('.history__data-list').innerHTML = '';
+      let arrHistory = JSON.parse('[' + localStorage.history + ']');
+      document.querySelector('.history__data-list').innerHTML = '';
 
-        arrHistory.forEach(item => {
-            let x = 'green';
-            item.typeOperation == '+' ? x : x = 'red';
+      arrHistory.forEach(item => {
+        let x = 'green';
+        item.typeOperation == '+' ? x : (x = 'red');
+        let icon =
+          item.typeOperation == '+' ? 'right-down-line' : 'left-up-line';
 
-            document.querySelector('.history__data-list').innerHTML += `
+        document.querySelector('.history__data-list').innerHTML += `
                 <div class="history__data-data">
                     <div class="history__data-date">${item.date}</div>
                     <div class="history__data-operation">
@@ -98,112 +102,118 @@ let progressBar = {
                         <div class="home__data-symbol usd">$</div>
                     </div>
                     <div class="history__data-operation">
-                        <div class="history__data-total">${item.remainingAmount}</div>
+                        <div class="history__data-total">
+                        <i class="ri-corner-${icon}"></i>
+                            ${item.remainingAmount}
+                        </div>
                         <div class="home__data-symbol uah">₴</div>
                     </div>
                 </div>
             `;
-        });
-    }
-};
+      });
+    },
+  };
 
-let btn = document.querySelectorAll('button');
+  const addCash = document.getElementById('plusCash');
+  const minusCash = document.getElementById('minusCash');
 
-btn.forEach(btn => {
-    btn.addEventListener('click', e => {
-        console.log(e.target.id);
-        if(cashInput.value === '') return; 
-        
-        e.target.id === 'plusCash' ?
-            progressBar.setCash() :
-            progressBar.remCash();
-    });
-});
+  addCash.addEventListener('click', () => {
+    if (cashInput.value === '') return;
+    progressBar.setCash();
+  });
 
-function saveDateLocalStor() {
+  minusCash.addEventListener('click', () => {
+    if (cashInput.value === '') return;
+    progressBar.remCash();
+  });
+
+  // SAVE DATA IN LOCAL STORAGE BROWSER
+  function saveDateLocalStor() {
     localStorage.totalSum = progressBar.totalSum;
     localStorage.remainingAmount = progressBar.remainingAmount;
-}
+  }
 
-function loadDateLocalStor() {
+  // LOAD DATA FROM LOCAL STORAGE BROWSER
+  function loadDateLocalStor() {
     if (localStorage.length == 0) return;
     progressBar.totalSum = +localStorage.totalSum;
     progressBar.remainingAmount = +localStorage.remainingAmount;
     progressBar.renderInfo();
-}
+  }
 
-function getCurrencyCourseDollarAPI() {
-    let url = 'https://va-backend.treeum.net/api/branches_rates?embedded={%22branch_id%22:1,%22profile_id%22:1}&max_results=50&page=1&where={%22branch_id%22:%22605a111b583f660a5c799b50%22,%22currency%22:%22usd%22}';
+  class HttpError extends Error {
+    constructor(message) {
+      super(message);
+      this.name = 'HttpError';
+    }
+  }
+
+  // GET CURRENT COURSE DOLLAR
+  function getCurrencyCourseDollarAPI() {
+    const url = 'https://api.monobank.ua/bank/currency';
 
     fetch(url)
-    .then((response) => {
+      .then(response => {
         if (response.ok) {
-            return response.json();
+          return response.json();
         }
-    
-        throw new Error(`Ошибка сетевого доступа, курс валют недоступен: ${response.status}`);
-    })
-    .then((data) => {
+
+        throw new HttpError(
+          `Ошибка сетевого доступа, курс валют недоступен: ${response.status}`,
+        );
+      })
+      .then(data => {
         parseObjectRate(data);
-    })
-    .catch((error) => {
-        showErrorMessage(error.message);
-    });
-}
+      })
+      .catch(error => {
+        if (error instanceof HttpError) {
+          console.log(error.message);
+          showErrorMessage(error.message);
+        }
+      });
+  }
 
-function parseObjectRate(data) {
-    // progressBar.rateSallUSD = data._items.rate.sell.value;
-
-    // console.log(data._items[0].rate.sell.value);
-    progressBar.rateSallUSD = data._items[0].rate.sell.value;
-    
-    // for (let item in data._items) {
-    //     if (data._items[item].data.currency === 'usd') {
-    //         progressBar.rateSallUSD = data._items.rate.sell.value;
-    //     }
-    // }
-
+  function parseObjectRate(data) {
+    progressBar.rateSallUSD = data[0].rateSell;
     progressBar.renderInfo();
-}
+  }
 
-
-const input = document.querySelector('input[type="text"]');
-
-input.addEventListener('input', () => {
+  // VALIDATION INPUT
+  const input = document.querySelector('input[type="text"]');
+  input.addEventListener('input', () => {
     input.value = input.value.replace(/\D/g, '');
-});
+  });
 
-
-// Save History Operation User
-
-function saveHistoryOperation() {
-
+  // SAVING USER ACTIVETY HISTORY
+  function saveHistoryOperation() {
     let objHistory = {
-        date: getDate(),
-        typeOperation: progressBar.typeOperation,
-        sumOperation: cashInput.value,
-        convertToUSD: progressBar.convertToUSD(),
-        remainingAmount: progressBar.totalSum,
+      date: getDate(),
+      typeOperation: progressBar.typeOperation,
+      sumOperation: cashInput.value,
+      convertToUSD: progressBar.convertToUSD(),
+      remainingAmount: progressBar.totalSum,
     };
 
     let obj = JSON.stringify(objHistory);
 
-    localStorage.history == undefined ?
-        localStorage.history = obj :
-        localStorage.history += ', ' + obj;
-}
+    localStorage.history == undefined
+      ? (localStorage.history = obj)
+      : (localStorage.history += ', ' + obj);
+  }
 
-function getDate() {
+  // GET CURRENT DATE
+  function getDate() {
     let date = new Date();
-    let d = date.getDate() + '.' + (+date.getMonth() + 1) + '.' + date.getFullYear();
+    let d =
+      date.getDate() + '.' + (+date.getMonth() + 1) + '.' + date.getFullYear();
     return d;
-}
+  }
 
+  // ERROR MESSAGE
+  let mainContainer = document.querySelector('.home__container');
+  console.log(mainContainer);
 
-let mainContainer = document.querySelector('.home__container');
-console.log(mainContainer);
-
-function showErrorMessage(errorCode) {
+  function showErrorMessage(errorCode) {
     let div = document.createElement('div');
     let theFirstChild = mainContainer.firstChild;
 
@@ -212,15 +222,22 @@ function showErrorMessage(errorCode) {
 
     mainContainer.appendChild(div);
     mainContainer.insertBefore(div, theFirstChild);
+  }
 
-    // setTimeout(() => {
-    //     document.querySelector('.error').remove();
-    // }, 5000);
-}
+  const btnClearProgress = document.getElementById('reset');
+  btnClearProgress.addEventListener('click', () => {
+    clearProgress();
+  });
 
-// showErrorMessage();
+  // RESET PROGRESS
+  function clearProgress() {
+    console.log('clear');
+    document.querySelector('.history__data-list').innerHTML = 'Нет историй';
+    progressBar.totalSum = 0;
+    localStorage.clear();
+    progressBar.renderInfo();
+  }
 
-loadDateLocalStor();
-getCurrencyCourseDollarAPI();
-
-// https://va-backend.treeum.net/api/search/applications_data?action=buy&profile_id=6058a050ee8f0319234fe6ff&size=60
+  loadDateLocalStor();
+  getCurrencyCourseDollarAPI();
+});
